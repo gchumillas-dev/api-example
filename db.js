524ad4ecbde597d154db466102a478751a047f1c
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-04-17 17:28:14 +0200

    set fake data

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
+const casual = require('casual')
+
 module.exports = () => {
   const data = { users: [] }
   // Create 1000 users
   for (let i = 0; i < 1000; i++) {
     data.users.push({
       id: i,
-      name: `user${i}`
+      fistName: casual.first_name,
+      lastName: casual.last_name,
+      age: casual.integer(21, 68),
+      email: casual.email,
+      profession: casual.title
     })
   }
   return data
