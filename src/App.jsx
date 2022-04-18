f0a6a591390a231827a597336d9ec57c4b1bf7e1
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-04-18 11:27:58 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,31 @@ function App() {
 
   return (
     <div>
-      {users.length}
-      <button onClick={loadUsers}>reload</button>
+      <p>
+        <button onClick={loadUsers}>reload</button>
+      </p>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Profession</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.age}</td>
+              <td>{user.email}</td>
+              <td>{user.profession}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
